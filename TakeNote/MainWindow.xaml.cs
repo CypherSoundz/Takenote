4825132f71869bcd1be015696b956b5b8f7f77b4
James <James>
2017-10-04 16:34:08 +0100

    first

--- TakeNote/MainWindow.xaml.cs
+++ TakeNote/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Google.Cloud.Speech.V1;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,26 @@ namespace TakeNote
         public MainWindow()
         {
             InitializeComponent();
+            dontJudgeMe();
+        }
+
+        private void dontJudgeMe()
+        {
+            var speech = SpeechClient.Create();
+            var response = speech.Recognize(new RecognitionConfig()
+            {
+                Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
+                SampleRateHertz = 44100,
+                LanguageCode = "en",
+            }, RecognitionAudio.FromFile(@"D:\once.wav"));
+            foreach (var result in response.Results)
+            {
+                foreach (var alternative in result.Alternatives)
+                {
+                    File.AppendAllText(@"D:\transcript.txt", alternative.Transcript);
+                    Console.WriteLine(alternative.Transcript);
+                }
+            }
         }
     }
 }
