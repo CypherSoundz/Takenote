5a7340222c5ddb2968de43a75d639baf6aa47bb1
James <James>
2017-10-04 16:55:57 +0100

    second with keyhook

--- TakeNote/MainWindow.xaml.cs
+++ TakeNote/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Google.Cloud.Speech.V1;
+﻿using Gma.System.MouseKeyHook;
+using Google.Cloud.Speech.V1;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -9,11 +10,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 
 namespace TakeNote
 {
@@ -22,12 +25,53 @@ namespace TakeNote
     /// </summary>
     public partial class MainWindow : Window
     {
+private IKeyboardMouseEvents m_GlobalHook;
         public MainWindow()
         {
             InitializeComponent();
-            dontJudgeMe();
+            hook();
+          //  dontJudgeMe();
         }
 
+        public void Subscribe()
+        {
+            // Note: for the application hook, use the Hook.AppEvents() instead
+            m_GlobalHook = Hook.GlobalEvents();
+
+            m_GlobalHook.MouseDownExt += GlobalHookMouseDownExt;
+            m_GlobalHook.KeyPress += GlobalHookKeyPress;
+        }
+
+        private void GlobalHookKeyPress(object sender, KeyPressEventArgs e)
+        {
+            Console.WriteLine("KeyPress: \t{0}", e.KeyChar);
+        }
+
+        private void GlobalHookMouseDownExt(object sender, MouseEventExtArgs e)
+        {
+            Console.WriteLine("MouseDown: \t{0}; \t System Timestamp: \t{1}", e.Button, e.Timestamp);
+
+            // uncommenting the following line will suppress the middle mouse button click
+            // if (e.Buttons == MouseButtons.Middle) { e.Handled = true; }
+        }
+
+        public void Unsubscribe()
+        {
+            m_GlobalHook.MouseDownExt -= GlobalHookMouseDownExt;
+            m_GlobalHook.KeyPress -= GlobalHookKeyPress;
+
+            //It is recommened to dispose it
+            m_GlobalHook.Dispose();
+        }
+
+        private void hook()
+        {
+            Subscribe();
+        }
+        private void RecordFromMic()
+        {
+          
+        }
         private void dontJudgeMe()
         {
             var speech = SpeechClient.Create();
